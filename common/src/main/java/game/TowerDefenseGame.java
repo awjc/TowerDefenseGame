5df83ae3b02bbba9f9d09a8424580c8b264957c1
Adam Campbell <a.wj.campbell@gmail.com>
2018-07-01 19:40:45 -0700

    Gradle ignore

--- common/src/main/java/game/TowerDefenseGame.java
+++ common/src/main/java/game/TowerDefenseGame.java
@@ -1,7 +1,17 @@
 package game;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class TowerDefenseGame {
-    public static void main(String[] args) {
-        JFrame f = new JFRame(1920, 1080);
-    }
+  public static void main(String[] args) {
+    // System.out.println("Hello, World");
+    JFrame f = new JFrame("My Game");
+    f.setSize(1920, 1080);
+    f.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+    f.setLocation((screenSize.width - f.getWidth()) / 2, (screenSize.height - f.getHeight()) / 2);
+
+    f.setVisible(true);
+  }
 }
